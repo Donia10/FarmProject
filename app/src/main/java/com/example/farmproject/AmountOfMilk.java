b83f502aa40b5ab58b27327900ae4f2c98dfe927
donia <donia.ashraf100@gmail.com>
2020-01-09 22:17:16 -0800

    add project

---
+++ app/src/main/java/com/example/farmproject/AmountOfMilk.java
@@ -0,0 +1,75 @@
+
+package com.example.farmproject;
+public class AmountOfMilk {
+    private String milkFirstTime;
+    private String milkSecondTime;
+    private String milkThirdTime;
+    private String fatPercentage;
+    private String proteinRatio;
+
+    public AmountOfMilk(String milkFirstTime, String milkSecondTime, String milkThirdTime, String fatPercentage, String proteinRatio) {
+        this.milkFirstTime = milkFirstTime;
+        this.milkSecondTime = milkSecondTime;
+        this.milkThirdTime = milkThirdTime;
+        this.fatPercentage = fatPercentage;
+        this.proteinRatio = proteinRatio;
+    }
+
+    public String getMilkFirstTime() {
+        return milkFirstTime;
+    }
+
+    public void setMilkFirstTime(String milkFirstTime) {
+        this.milkFirstTime = milkFirstTime;
+    }
+
+    public String getMilkSecondTime() {
+        return milkSecondTime;
+    }
+
+    public void setMilkSecondTime(String milkSecondTime) {
+        this.milkSecondTime = milkSecondTime;
+    }
+
+    public String getMilkThirdTime() {
+        return milkThirdTime;
+    }
+
+    public void setMilkThirdTime(String milkThirdTime) {
+        this.milkThirdTime = milkThirdTime;
+    }
+
+    public String getFatPercentage() {
+        return fatPercentage;
+    }
+
+    public void setFatPercentage(String fatPercentage) {
+        this.fatPercentage = fatPercentage;
+    }
+
+    public String getProteinRatio() {
+        return proteinRatio;
+    }
+
+    public void setProteinRatio(String proteinRatio) {
+        this.proteinRatio = proteinRatio;
+    }
+
+}
+ class AmoutOfMilk {
+
+    private AmountOfMilk amountOfMilk;
+
+     public AmoutOfMilk(AmountOfMilk amountOfMilk) {
+         this.amountOfMilk = amountOfMilk;
+     }
+
+     public AmountOfMilk getAmountOfMilk() {
+        return amountOfMilk;
+    }
+
+    public void setAmountOfMilk(AmountOfMilk amountOfMilk) {
+        this.amountOfMilk = amountOfMilk;
+    }
+
+}
---
+++ app/src/main/java/com/example/farmproject/Milk.java
@@ -0,0 +1,131 @@
+package com.example.farmproject;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class Milk extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    EditText milkTheFirstTime;
+    EditText milkTheSecondTime;
+    EditText milkTheThirdTime;
+    EditText fatText;
+    EditText proteinText;
+    Button add;
+    //firebase instance object
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mAnimalsDatabaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_milk);
+
+        milkTheFirstTime=findViewById(R.id.item1);
+        milkTheSecondTime=findViewById(R.id.item2);
+        milkTheThirdTime=findViewById(R.id.item3);
+        fatText=findViewById(R.id.fat);
+        proteinText=findViewById(R.id.protein);
+        add=findViewById(R.id.add);
+        //firebase object
+        mFirebaseDatabase= FirebaseDatabase.getInstance();
+        mAnimalsDatabaseReference=mFirebaseDatabase.getReference().child("animals");
+       //toolbar
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("كمية إدرار اللبن");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setTitleTextColor(Color.WHITE);
+        //spinner
+        final Spinner spinner=findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.items, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AmountOfMilk amountOfMilk=new AmountOfMilk(milkTheFirstTime.getText().toString(),milkTheSecondTime.getText().toString(),milkTheThirdTime.getText().toString(),fatText.getText().toString(),proteinText.getText().toString());
+
+                //create new node in tree of animals
+              /* Map<String,AmountOfMilk> milkMap= new HashMap<>();
+                Map<String,String>map=new HashMap<>();
+                milkMap.put("Milk",amountOfMilk);
+                milkMap.put("add",amountOfMilk);**/
+                //mAnimalsDatabaseReference.setValue(milkMap);
+                //create milk into id
+               // mAnimalsDatabaseReference.push().setValue(milkMap);
+
+                //create milk into id without Map
+                mAnimalsDatabaseReference.push().child("milk").setValue(amountOfMilk);
+
+                //mAnimalsDatabaseReference.setValue(amountOfMilk);
+              // mAnimalsDatabaseReference.push().setValue(amountOfMilk);
+                //milkTheFirstTime.setText("0", TextView.BufferType.EDITABLE); /*// May be unecessary... */
+                milkTheFirstTime.getText().clear();
+                milkTheSecondTime.getText().clear();
+                milkTheThirdTime.getText().clear();
+                fatText.getText().clear();
+                proteinText.getText().clear();
+                //fatText.setText("0");
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(position==1){
+            milkTheFirstTime.setVisibility(View.VISIBLE);
+            milkTheSecondTime.setVisibility(View.GONE);
+            milkTheThirdTime.setVisibility(View.GONE);
+           //apply data
+           /* AmountOfMilk amountOfMilk=new AmountOfMilk(editText1.getText().toString(),null,null,fatText.getText().toString(),proteinText.getText().toString());
+            mAnimalsDatabaseReference.push().setValue(amountOfMilk);**/
+        }else if(position==2){
+            milkTheFirstTime.setVisibility(View.VISIBLE);
+            milkTheSecondTime.setVisibility(View.VISIBLE);
+            milkTheThirdTime.setVisibility(View.GONE);
+
+
+/*
+            AmountOfMilk amountOfMilk=new AmountOfMilk(editText1.getText().toString(),editText2.getText().toString(),null,fat.getText().toString(),protein.getText().toString());
+            mAnimalsDatabaseReference.push().setValue(amountOfMilk);**/
+        }else if(position==3){
+            milkTheFirstTime.setVisibility(View.VISIBLE);
+            milkTheSecondTime.setVisibility(View.VISIBLE);
+            milkTheThirdTime.setVisibility(View.VISIBLE);
+           /* AmountOfMilk amountOfMilk=new AmountOfMilk(editText1.getText().toString(),editText2.getText().toString(),editText3.getText().toString(),fat.getText().toString(),protein.getText().toString());
+            mAnimalsDatabaseReference.push().setValue(amountOfMilk);**/
+
+        }else {
+            milkTheFirstTime.setVisibility(View.GONE);
+            milkTheSecondTime.setVisibility(View.GONE);
+            milkTheThirdTime.setVisibility(View.GONE);
+
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
