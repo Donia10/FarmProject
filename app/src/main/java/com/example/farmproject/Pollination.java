c8f2fd88e0a1b66d235dd41d702a393545a47432
asmaa <asmaaghoname204@gmail.com>
2020-02-25 00:51:11 +0200

    database commit

--- app/src/main/java/com/example/farmproject/Pollination.java
+++ app/src/main/java/com/example/farmproject/Pollination.java
@@ -48,37 +48,49 @@ public class Pollination extends AppCompatActivity implements AdapterView.OnItem
         number_poll.setAdapter(newadapter);
         number_poll.setOnItemSelectedListener(this);
 
-      /*  final ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.Spinner8_items, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.Spinner8_items, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         result.setAdapter(newadapter);
-        result.setOnItemSelectedListener(this);*/
+        result.setOnItemSelectedListener(this);
 
-     /*  add.setOnClickListener(new View.OnClickListener() {
+       add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // writeOnDatabase();
+               writeOnDatabase();
 
             }
-        }); */
+        });
     }
 
    @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-       // parent.getItemAtPosition(position);
-       number_poll.setVisibility(View.VISIBLE);
+     // number_poll.setVisibility(View.VISIBLE);
+       if(parent.getId() == R.id.number_pollination) {
+           parent.getItemAtPosition(position);
+
+       }
+       else if(parent.getId() == R.id.result_poll)
+       {
+           parent.getItemAtPosition(position);
+
+       }
+
+
+   }
 
 
 
-    }
+
+
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
-  /*  public void writeOnDatabase(){
+   public void writeOnDatabase(){
         Pllinations pollinations=new  Pllinations(number_poll.getSelectedItem().toString(), result.getSelectedItem().toString());
         mAnimalsDatabaseReference.child("pollinations").child("1").setValue(pollinations);
 
-    }*/
+    }
 }
